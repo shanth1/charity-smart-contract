7951124f325c94717afd15f11e4776023f1733d1
Denis Beresnev <shanthi.bunch@gmail.com>
2024-04-08 04:18:23 +0300

    refactor: reduce gas amount and code style

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -1,52 +1,40 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract Donation {
-	uint public goal;
-	bool public goalAchieved;
+  uint256 public goal;
+  uint256 public totalDonated;
+  address payable public receiver;
 
-	uint public totalDonated;
-	address payable public receiver;
+  event Donate(address indexed donor, uint256 amount, uint256 totalDonated);
+  event GoalReached(uint256 totalDonated);
 
-	event Donate(address donor, uint amount, uint totalDonated);
-  event GoalReached(uint totalDonated);
-
-	constructor(uint _goal, address payable _receiver) {
-		require(_goal > 0, "Goal must be greater than 0");
+  constructor(uint256 _goal, address payable _receiver) {
+    require(_goal > 0, "Goal must be greater than 0");
     goal = _goal;
     receiver = _receiver;
-    totalDonated = 0;
-    goalAchieved = false;
-	}
-
-	function donate(bool shouldRefund) external payable {
-		require(!goalAchieved, "Donation goal already reached");
-		require(msg.value > 0, "Donation must be greater than 0");
-
-		uint amountToDonate = msg.value;
-		if(totalDonated + msg.value > goal) {
-			amountToDonate = goal - totalDonated;
-			goalAchieved = true;
-			emit GoalReached(totalDonated + amountToDonate);
-		}
-
-		if(shouldRefund) {
-			 payable(msg.sender).transfer(msg.value - amountToDonate);
-		}
-
-		totalDonated += amountToDonate;
-    emit Donate(msg.sender, amountToDonate, totalDonated);
-
-    if(goalAchieved) {
-      receiver.transfer(totalDonated);
-    }
-	}
-
-	function isGoalReached() public view returns (bool) {
- 		return goalAchieved;
   }
 
-  function getTotalDonated() public view returns (uint) {
-  	return totalDonated;
+  function donate(bool shouldRefund) external payable {
+    require(totalDonated < goal, "Donation goal already reached");
+    require(msg.value > 0, "Donation must be greater than 0");
+
+    uint256 amountToDonate = msg.value;
+    if (totalDonated + msg.value >= goal) {
+      amountToDonate = goal - totalDonated;
+      emit GoalReached(goal);
+    }
+
+    if (shouldRefund && msg.value > amountToDonate) {
+      uint256 refundAmount = msg.value - amountToDonate;
+      payable(msg.sender).transfer(refundAmount);
+    }
+
+    emit Donate(msg.sender, amountToDonate, totalDonated);
+    totalDonated += amountToDonate;
+
+    if (totalDonated >= goal) {
+      receiver.transfer(totalDonated);
+    }
   }
 }
